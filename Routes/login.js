17d0de3ebe34367fec2cb4793d886ca89c6d3b08
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-08-29 17:59:36 +0100

    Update login.js

--- Routes/login.js
+++ Routes/login.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const _ = require('lodash');
 const mongoose = require('mongoose');
 const router = express.Router();
-const {User, validate}= require('../Model/User');
+const {User, validation}= require('../Model/User');
 const  Joi = require('joi');
 
 
 router.post("/login", async(req, res)=>{
-    const { error } = validate(req.body);
+    const { error } = validation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     
-    const login = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('User not registered please sign up');
+
+    const login = new User(_.pick(req.body), ['email', 'password']);
     try {
         const result = await login;
         console.log(result);
