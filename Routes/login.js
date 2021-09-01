c997bf833956d184a05f869ec3d85f88b81dad53
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-09-01 14:01:36 +0100

    Update login.js

--- Routes/login.js
+++ Routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const router = express.Router();
 const {User, validation}= require('../Model/User');
@@ -12,16 +13,15 @@ router.post("/login", async(req, res)=>{
     
     let user = await User.findOne({email: req.body.email});
     if (!user) return res.status(400).send('User not registered please sign up');
+     
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid password');
+    
+    const token = user.generateAuthToken();
+    res.send(token);
 
-    const login = new User(_.pick(req.body), ['email', 'password']);
-    try {
-        const result = await login;
-        console.log(result);
-        res.send("logged in");
-        }
-        catch (ex) {
-        console.log(ex.message);
-    };
+
+
 });
 
 
