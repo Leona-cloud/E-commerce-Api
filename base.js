d740747a5d0d1795c4fbb6094a8ee8642d22c113
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-09-01 14:00:46 +0100

    Update base.js

--- base.js
+++ base.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const config = require('config');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const users = require('./Routes/Users');
@@ -15,12 +16,12 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api/users', users);
 app.use('/api/user', login);
 
+if(!config.get('jwtPrivateKey')){
+    console.log('Fatal Error : jwtPrivateKey is not defined');
+    process.exit(1);
+}
 
 dotenv.config();
-
-
-
-
 
 mongoose.connect(process.env.DB_CONNECT, 
 {useNewUrlParser: true, 
