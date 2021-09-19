56572fdc1d06be88d87a7a6a2c67a8aa70719c1f
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-09-19 21:19:40 +0100

    Update product.js

--- Routes/product.js
+++ Routes/product.js
@@ -2,8 +2,8 @@ const auth = require('../middleware/auth')
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const router = require('./Users');
-const { Product } = require('../Model/Product')
+const router = express.Router();
+const { Product, validP } = require('../Model/Product')
 
 router.get('/', async(req, res)=>{
     
@@ -12,15 +12,31 @@ router.get('/', async(req, res)=>{
 });
 
 router.post('/', async(req, res)=>{
+    const { error } = validP(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     try{
-    let product = new Product(_.pick(req.body, ['productName', 'amountAvail', 'description', 'Price', 'discount']));
+    let product = new Product(_.pick(req.body, ['productName', 'quantityInStock', 'description', 'price', 'discount', 'color']));
     product = await product.save();
     res.send(product)
     console.log(product)
     }catch(ex){
         console.log(ex)
     }
+});
+
+router.put('/:id', async(req, res)=>{
+    const { error } = validP(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let product = await Product.findByIdAndUpdate(req.params.id, {productName: req.body.productName},{
+        new: true
+    });
+    if(!product) return res.status(400).send("Product not found");
+    res.send(product);
 })
+
+
 
 
 module.exports = router;
