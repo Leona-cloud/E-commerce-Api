cbec0a794aed0f33a39effeb65eea480838afc1d
Leona-cloud <60227761+Leona-cloud@users.noreply.github.com>
2021-09-19 21:13:51 +0100

    Update Product.js

--- Model/Product.js
+++ Model/Product.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const productSchema = new mongoose.Schema({
+    
     productName: {
         type: String,
         required: true,
     },
-    amountAvail: { // quantityInStock :
+    quantityInStock: {
         type: Number,
         required: true
     },
@@ -14,16 +17,39 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    Price: {
-        type: String,
+    price: {
+        type: Number,
         required: true,
     },
     discount: {
         type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
     }
-});
+   
+},{timestamps: true});
+productSchema.methods.generateAuthToken = function (){
+    const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'));
+    return token;
+}
 
 const Product = mongoose.model('Product', productSchema);
 
 
+function productValidation(product){
+    const schema = Joi.object({
+        productName: Joi.string().required().trim().lowercase(),
+        amountAvail: Joi.string().required().min(0).max(12),
+        description: Joi.string().required(),
+        price: Joi.string().required()
+    })
+    return schema.validate(product)
+};
+
+
 module.exports.Product = Product;
+
+module.exports.validP = productValidation;
