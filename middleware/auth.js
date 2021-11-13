a4192a4aea38bd462ab5f2871e0bfbea35865b49
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-11-14 00:14:53 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const dotenv = require('dotenv').config();
 
 function auth(req, res, next){
     const token = req.header('x-auth-token');
     if(!token) res.status(401).send('Access denied. no token provided');
     try{
-        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        const decoded = jwt.verify(token, process.env.ecommerce_jwtPrivateKey);
         req.user = decoded;
         next();
     }catch(ex){
