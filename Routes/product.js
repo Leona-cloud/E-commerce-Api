3ada7345561a2ceea2426f97ee1ef5e22f611a6a
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-11-15 21:48:48 +0100

    Role based authorization

--- Routes/product.js
+++ Routes/product.js
@@ -1,40 +1,98 @@
-const auth = require('../middleware/auth')
 const _ = require('lodash');
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const admin = require('../middleware/admin');
+const auth = require('../middleware/auth')
+const express = require('express');
 const router = express.Router();
-const { Product, validP } = require('../Model/Product')
+const { Product } = require('../Model/Product');
+const { Category } = require('../Model/category');
 
-router.get('/', async(req, res)=>{
-    
-        const products = await Product.find().sort('productName')
-        res.send(products)
+
+
+router.get(`/`, auth,async (req, res) =>{
+ 
+    let filter = {};
+    if(req.query.categories)
+    {
+         filter = {category: req.query.categories.split(',')}
+    }
+
+    const productList = await Product.find(filter).populate('category');
+ 
+    res.send(productList);
+})
+
+router.get('/:id', auth,  async(req, res)=>{
+    const product = await Product.findById(req.params.id).populate('category');
+    if(!product) return res.status(400).json({success: false, message: "Product with the given id doesn't exist "});
+
+    res.send(product)
 });
 
-router.post('/', async(req, res)=>{
-    const { error } = validP(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post('/', [auth, admin], async(req, res)=>{
+    const category = await Category.findById(req.body.category)
+    if(!category) return res.status(400).send('Invalid category');
 
-    try{
-    let product = new Product(_.pick(req.body, ['productName', 'quantityInStock', 'description', 'price', 'discount', 'color']));
-    product = await product.save();
-    res.send(product)
-    console.log(product)
-    }catch(ex){
-        console.log(ex)
+    let product = new Product(_.pick(req.body, ['name','description', ' richDescription', ' image', 'brand','price','countInStock', 'category','rating','isFeatured','numReviews' ]))
+     
+    try {
+        product = await product.save()
+        res.send(product)
+    } catch (err) {
+        res.status(400).json({success: false, message: err})
     }
 });
 
-router.put('/:id', async(req, res)=>{
-    const { error } = validP(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.put('/:id', [auth, admin] , async(req, res)=>{
+    const category = await Category.findById(req.params.category)
+    if(!category) return res.status(400).send('Invalid category');
 
-    let product = await Product.findByIdAndUpdate(req.params.id, {productName: req.body.productName},{
-        new: true
-    });
-    if(!product) return res.status(400).send("Product not found");
-    res.send(product);
-})
+    let product = await Product.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        countInStock: req.body.countInStock,
+        category: req.body.category,
+        rating: req.body.rating,
+        isFeatured: req.body.isFeatured,
+        numReviews: req.body.numReviews
+    }, {new: true})
+    try {
+        if(!product) return res.status(400).send('This product does not exist')
+        res.send(product)
+    } catch (err) {
+        res.status(400).json({success: false, message: err})
+    }
+});
+
+router.delete('/:id', [auth,admin] , async(req, res)=>{
+   
+    let product = await Product.findByIdAndRemove(req.params.id)
+    try{  if (!product) {
+        return res.status(404).json({success: false, message: "product with the given id does not exist"});
+      } else {
+       return res.json({success: true, message: "product has been deleted"});
+      }
+    }catch(err){
+        res.status(400).json({success: false, message: err})
+    }
+  
+});
+
+router.get('/get/featured/:count', auth, async(req, res)=>{
+    const count = req.params.count ? req.params.count: 0
+    const products = await Product.find({isFeatured: true}).limit(+count);
+
+    if(!products) return res.status(500).json({success: false, message: 'not featured'});
+
+    res.send(products)
+});
+
+
+
 
 
 
