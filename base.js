6f43e32b5916feb188b6c106a8899db38b7e7880
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-11-19 22:38:12 +0100

    added order route

--- base.js
+++ base.js
@@ -1,39 +1,37 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const users = require('./Routes/Auth');
-const product = require('./Routes/product');
-const categories = require('./Routes/categories');
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const users = require("./Routes/Auth");
+const product = require("./Routes/product");
+const categories = require("./Routes/category");
+const order = require("./Routes/order");
 
 const app = express();
 
-if(!process.env.ecommerce_jwtPrivateKey){
-    console.error('FATAL ERROR: jwtPrivateKey not defined');
-    process.exit(1)
+if (!process.env.ecommerce_jwtPrivateKey) {
+  console.error("FATAL ERROR: jwtPrivateKey not defined");
+  process.exit(1);
 }
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
-app.use('/api/auth', users);
-app.use('/api/categories', categories)
-app.use('/api/product', product);
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", users);
+app.use("/api/categories", categories);
+app.use("/api/product", product);
+app.use("/api/order", order);
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT, 
-{useNewUrlParser: true, 
-    useUnifiedTopology: true})
-    .then(()=> console.log("connected"))
-    .catch(err => console.error("unable to connect", err));
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected"))
+  .catch((err) => console.error("unable to connect", err));
 
-
-
-
-
-const port = process.env.PORT || 3000
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}.....`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}.....`);
 });
